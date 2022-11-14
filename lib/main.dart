7a1c0d376427baac839c98486312dc2d85c96b76
MisbahAli <smisbahali@gmail.com>
2022-11-14 15:37:18 +0500

    themes extracted

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,20 @@ import 'package:fone/pages/login_page.dart';
 import 'package:fone/pages/home_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:fone/pages/routes.dart';
-
+import 'widgets/themes.dart';
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //const MyApp({required Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      initialRoute: "/", // in start got to home page
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
